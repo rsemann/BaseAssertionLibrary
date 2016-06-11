4f7acd968cf192c6242a34dd65b92a2ddb80f35b
rsemann <semann.rafael@gmail.com>
2016-06-11 11:50:09 -0300

    Custom exception

---
+++ BaseAssertionLibrary/BaseAssertionLibrary/Library/ExpectationFailedExceptin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Unit.Library
+{
+    public class ExpectationFailedExceptin : Exception
+    {
+        public ExpectationFailedExceptin()
+        {
+        }
+
+        public ExpectationFailedExceptin(string message)
+            : base(message)
+        {
+        }
+
+        public ExpectationFailedExceptin(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
